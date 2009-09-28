5f280e1d349646bec4be3659a295cce3fa3fe264
Dr Nic Williams <drnicwilliams@gmail.com>
2009-09-29 08:26:05 +1000

    using before(:all) around the theme installation calls so they are only run once

--- spec/install_theme_haml_spec.rb
+++ spec/install_theme_haml_spec.rb
@@ -2,7 +2,7 @@ require File.dirname(__FILE__) + '/spec_helper.rb'
 
 describe InstallTheme do
   context "bloganje theme to haml" do
-    before do
+    before(:all) do
       setup_base_rails :haml => true
       @stdout = stdout do |stdout|
         @theme = InstallTheme.new(:template_root => File.dirname(__FILE__) + "/fixtures/bloganje", 
@@ -14,35 +14,33 @@ describe InstallTheme do
       end
       @expected_application = File.dirname(__FILE__) + "/expected/rails/bloganje"
     end
-    describe "becomes a Rails app with html templates" do
-      it { @theme.should be_haml }
-      it { File.should be_exist(File.join(@expected_application, "app/views/layouts/application.html.haml")) }
-      it { File.should be_exist(File.join(@target_application, "app/views/layouts/application.html.haml")) }
-      it { File.should_not be_exist(File.join(@target_application, "app/views/layouts/application.html.erb")) }
-      it "should create app/views/layouts/application.html.erb as a layout file" do
-        expected = clean_html(File.join(@expected_application, "app/views/layouts/application.html.haml"))
-        actual   = clean_html(File.join(@target_application, "app/views/layouts/application.html.haml"))
-        diff = `diff #{expected} #{actual}  2> /dev/null`
-        rputs diff unless diff.strip.empty?
-        diff.strip.should == ""
+    it { @theme.should be_haml }
+    it { File.should be_exist(File.join(@expected_application, "app/views/layouts/application.html.haml")) }
+    it { File.should be_exist(File.join(@target_application, "app/views/layouts/application.html.haml")) }
+    it { File.should_not be_exist(File.join(@target_application, "app/views/layouts/application.html.erb")) }
+    it "should create app/views/layouts/application.html.erb as a layout file" do
+      expected = clean_html(File.join(@expected_application, "app/views/layouts/application.html.haml"))
+      actual   = clean_html(File.join(@target_application, "app/views/layouts/application.html.haml"))
+      diff = `diff #{expected} #{actual}  2> /dev/null`
+      rputs diff unless diff.strip.empty?
+      diff.strip.should == ""
+    end
+    
+    it { File.should be_exist(File.join(@target_application, "public/stylesheets/sass/style.sass")) }
+    it { File.should be_exist(File.join(@expected_application, "public/stylesheets/sass/style.sass")) }
+    it do
+      expected = File.join(@expected_application, "public/stylesheets/sass/style.sass")
+      actual   = File.join(@target_application, "public/stylesheets/sass/style.sass")
+      diff = `diff #{expected} #{actual}  2> /dev/null`
+      rputs diff unless diff.strip.empty?
+      diff.strip.should == ""
+    end
+    context "sample template /original_template/index.html.haml" do
+      subject do
+        File.read(File.join(@target_application, 'app/views/original_template/index.html.haml'))
       end
-      
-      it { File.should be_exist(File.join(@target_application, "public/stylesheets/sass/style.sass")) }
-      it { File.should be_exist(File.join(@expected_application, "public/stylesheets/sass/style.sass")) }
-      it do
-        expected = File.join(@expected_application, "public/stylesheets/sass/style.sass")
-        actual   = File.join(@target_application, "public/stylesheets/sass/style.sass")
-        diff = `diff #{expected} #{actual}  2> /dev/null`
-        rputs diff unless diff.strip.empty?
-        diff.strip.should == ""
-      end
-      context "sample template /original_template/index.html.haml" do
-        subject do
-          File.read(File.join(@target_application, 'app/views/original_template/index.html.haml'))
-        end
-        it { should include("- content_for :head do\n  %script") }
-        it { should include("- content_for :header do\n  My eCommerce Admin area") }
-      end
+      it { should include("- content_for :head do\n  %script") }
+      it { should include("- content_for :header do\n  My eCommerce Admin area") }
     end
   end
 end
--- spec/install_theme_spec.rb
+++ spec/install_theme_spec.rb
@@ -2,59 +2,54 @@ require File.dirname(__FILE__) + '/spec_helper.rb'
 
 describe InstallTheme do
   context "bloganje theme to ERb" do
-    before do
+    before(:all) do
       setup_base_rails
-      stdout do |stdout|
+      @stdout = stdout do |stdout|
         @theme = InstallTheme.new(:template_root => File.dirname(__FILE__) + "/fixtures/bloganje", 
           :rails_root    => @target_application,
           :content_id    => "content",
           :inside_yields => { :header => '#header h2', :sidebar => '#sidebar' },
           :stdout        => stdout)
+        @theme.apply_to_target(:stdout => stdout, :generator => {:collision => :force, :quiet => true})
       end
       @expected_application = File.dirname(__FILE__) + "/expected/rails/bloganje"
+      puts "before(:all)"
     end
     it { @theme.stylesheet_dir.should == "css" }
     it { @theme.image_dir.should == "img" }
     it { @theme.should be_erb }
-    describe "becomes a Rails app with html templates" do
-      before do
-        @stdout = stdout do |stdout|
-          @theme.apply_to_target(:stdout => stdout, :generator => {:collision => :force, :quiet => true})
+    it { File.should be_exist(File.join(@target_application, "app/views/layouts/application.html.erb")) }
+    it { File.should be_exist(File.join(@expected_application, "app/views/layouts/application.html.erb")) }
+    it "should create app/views/layouts/application.html.erb as a layout file" do
+      expected = clean_html(File.join(@expected_application, "app/views/layouts/application.html.erb"))
+      actual   = File.join(@target_application, "app/views/layouts/application.html.erb")
+      diff = `diff #{expected} #{actual}  2> /dev/null`
+      rputs diff unless diff.strip.empty?
+      diff.strip.should == ""
+    end
+    
+    %w[public/stylesheets/style.css
+      public/stylesheets/theme.css].each do |matching_file|
+        it { File.should be_exist(File.join(@target_application, matching_file)) }
+        it do
+          expected = File.join(@expected_application, matching_file)
+          actual   = File.join(@target_application, matching_file)
+          diff = `diff #{expected} #{actual}  2> /dev/null`
+          rputs diff unless diff.strip.empty?
+          diff.strip.should == ""
         end
       end
-      it { File.should be_exist(File.join(@target_application, "app/views/layouts/application.html.erb")) }
-      it { File.should be_exist(File.join(@expected_application, "app/views/layouts/application.html.erb")) }
-      it "should create app/views/layouts/application.html.erb as a layout file" do
-        expected = clean_html(File.join(@expected_application, "app/views/layouts/application.html.erb"))
-        actual   = File.join(@target_application, "app/views/layouts/application.html.erb")
-        diff = `diff #{expected} #{actual}  2> /dev/null`
-        rputs diff unless diff.strip.empty?
-        diff.strip.should == ""
+    context "sample template /templates/index.html.erb" do
+      subject do
+        File.read(File.join(@target_application, 'app/views/original_template/index.html.erb'))
       end
-      
-      %w[public/stylesheets/style.css
-        public/stylesheets/theme.css].each do |matching_file|
-          it { File.should be_exist(File.join(@target_application, matching_file)) }
-          it do
-            expected = File.join(@expected_application, matching_file)
-            actual   = File.join(@target_application, matching_file)
-            diff = `diff #{expected} #{actual}  2> /dev/null`
-            rputs diff unless diff.strip.empty?
-            diff.strip.should == ""
-          end
-        end
-      context "sample template /templates/index.html.erb" do
-        subject do
-          File.read(File.join(@target_application, 'app/views/original_template/index.html.erb'))
-        end
-        it { should include("<% content_for :head do -%>\n  <script></script>\n<% end -%>") }
-        it { should include("<% content_for :header do -%>\n  My eCommerce Admin area\n<% end -%>") }
-      end
+      it { should include("<% content_for :head do -%>\n  <script></script>\n<% end -%>") }
+      it { should include("<% content_for :header do -%>\n  My eCommerce Admin area\n<% end -%>") }
     end
   end
 
   context "the-hobbit-website-template theme to ERb" do
-    before do
+    before(:all) do
       setup_base_rails
       @stdout = stdout do |stdout|
         @theme = InstallTheme.new(
