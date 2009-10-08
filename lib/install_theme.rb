9c0c50e6a71850c7a9692597ce949429b3f30a4a
Dr Nic Williams <drnicwilliams@gmail.com>
2009-10-08 23:31:23 +1000

    bump to 0.7.1

--- lib/install_theme.rb
+++ lib/install_theme.rb
@@ -10,7 +10,7 @@ require 'sass/css'
 require 'haml/exec'
 
 class InstallTheme
-  VERSION = "0.7.0"
+  VERSION = "0.7.1"
   
   attr_reader :template_root, :rails_root, :index_path, :template_type
   attr_reader :layout_name
